16866c72f328a75ccfd6a97a2bbeeb6b304473e0
muskaans31 <suranamuskaan318@gmail.com>
2020-10-29 19:25:48 +0530

    Platform added

--- sketch.js
+++ sketch.js
@@ -22,6 +22,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    platform=new Ground(180,305,300,170);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -57,6 +58,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platform.display();
     bird.display();
 }
